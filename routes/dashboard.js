2bf3836bbaa68e7539d81e95f0284cbf4332c054
FarazHussain2163 <faraz.hussain@bargoventures.com>
2023-02-12 21:05:47 +0500

    changes done

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,123 +4,149 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const User = require("../model/user");
 const Order = require("../model/order");
-const moment = require('moment');
+const moment = require("moment");
+const order = require("../model/order");
+const post = require("../model/post");
 
 router.get("/dashboard", auth, async (req, res) => {
-    const { query } = req;
-    const year = query.year;
-    const graph = await getGraphData(year);
-    const packageData = await getPopularPackageData();
-    res.status(200).send({
-        graph,
-        packageData
-    });
+  const { query } = req;
+  const year = query.year;
+  const graph = await getGraphData(year);
+  const packageData = await getPopularPackageData();
+  res.status(200).send({
+    graph,
+    packageData,
+  });
 });
 
-const getGraphData = async year => {
-    const arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    try {
-        const start_date = moment().set({ year }).startOf("year").toDate();
-        const end_date = moment().set({ year }).endOf("year").toDate();
-        const query = [
-            {
-                $match: {
-                    createdAt: {
-                        $gte: start_date,
-                        $lte: end_date,
-                    },
-                },
-            },
-            {
-                $addFields: {
-                    date: {
-                        $month: "$createdAt",
-                    },
-                },
-            },
-            {
-                $group: {
-                    _id: "$date",
-                    count: { $sum: 1 },
-                },
-            },
-            {
-                $addFields: {
-                    month: "$_id",
-                },
-            },
-            {
-                $project: {
-                    _id: 0,
-                    month: 1,
-                    count: 1,
-                },
-            },
-        ];
-        const user_data = await User.aggregate(query);
-        user_data.forEach((data) => {
-            if (data) arr[data.month - 1] = data.count;
-        });
-        return arr;
-    } catch (err) {
-        console.log(err)
-        return arr;
-    }
-}
+const getGraphData = async (year) => {
+  const arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  try {
+    const start_date = moment().set({ year }).startOf("year").toDate();
+    const end_date = moment().set({ year }).endOf("year").toDate();
+    const query = [
+      {
+        $match: {
+          createdAt: {
+            $gte: start_date,
+            $lte: end_date,
+          },
+        },
+      },
+      {
+        $addFields: {
+          date: {
+            $month: "$createdAt",
+          },
+        },
+      },
+      {
+        $group: {
+          _id: "$date",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $addFields: {
+          month: "$_id",
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          month: 1,
+          count: 1,
+        },
+      },
+    ];
+    const user_data = await User.aggregate(query);
+    user_data.forEach((data) => {
+      if (data) arr[data.month - 1] = data.count;
+    });
+    return arr;
+  } catch (err) {
+    console.log(err);
+    return arr;
+  }
+};
 
-const getPopularPackageData = async year => {
-    try {
-        const start_date = moment().set({ year }).startOf("year").toDate();
-        const end_date = moment().set({ year }).endOf("year").toDate();
-        const query = [
-            {
-                $match: {
-                    createdAt: {
-                        $gte: start_date,
-                        $lte: end_date,
-                    },
-                },
-            },
-            {
-                $addFields: {
-                    packageName: "$pkg_name",
-                },
-            },
-            {
-                $group: {
-                    _id: "$pkg_name",
-                    count: { $sum: 1 },
-                },
-            },
-            {
-                $project: {
-                    _id: 1,
-                    packageName: 1,
-                    count: 1,
-                },
-            },
-        ];
-        const packageData = await Order.aggregate(query);
-        
-        const packages = ["Bronze", "Silver", "Gold"];
+const getPopularPackageData = async (year) => {
+  try {
+    const start_date = moment().set({ year }).startOf("year").toDate();
+    const end_date = moment().set({ year }).endOf("year").toDate();
+    const query = [
+      {
+        $match: {
+          createdAt: {
+            $gte: start_date,
+            $lte: end_date,
+          },
+        },
+      },
+      {
+        $addFields: {
+          packageName: "$pkg_name",
+        },
+      },
+      {
+        $group: {
+          _id: "$pkg_name",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          packageName: 1,
+          count: 1,
+        },
+      },
+    ];
+    const packageData = await Order.aggregate(query);
 
-        return packages.map( (pkg, idx) => {
-            const index = packageData.findIndex(pd => pd._id == pkg);
+    const packages = ["Bronze", "Silver", "Gold"];
 
-            if(index < 0) {
-                return {
-                    _id: pkg,
-                    count: 0,
-                }
-            }
-            return packageData[index];
-        })
+    return packages.map((pkg, idx) => {
+      const index = packageData.findIndex((pd) => pd._id == pkg);
 
-        return packageData;
-    } catch (err) {
-        console.log(err)
-        return arr;
-    }
-}
+      if (index < 0) {
+        return {
+          _id: pkg,
+          count: 0,
+        };
+      }
+      return packageData[index];
+    });
+
+    return packageData;
+  } catch (err) {
+    console.log(err);
+    return arr;
+  }
+};
+
+router.get("/user-dashboard", auth, async (req, res) => {
+  try {
+    const UserId = req.user.user_id;
+    const subscribedPackages = (
+      await order.find({ status: "Active", user: UserId })
+    ).length;
+    const posts = await post.find({ user: UserId });
+    const approvedPosts = posts.filter((p) => p.status === "Approved").length;
+    const pendingPosts = posts.filter(
+      (p) => p.status === "Pending Approval"
+    ).length;
+    const dashboardDataToSend = {
+      subscribedPackages,
+      approvedPosts,
+      pendingPosts,
+    };
+    console.log(dashboardDataToSend);
+    return res.status(200).json(dashboardDataToSend);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
+});
 
 module.exports = router;
