7d3a13d55b6c36a0655683b10360c6f8d0b29ace
RijaNadeem <rija.nadeem@cloudprimero.com>
2023-06-25 19:42:53 +0500

    link update

--- model/package.js
+++ model/package.js
@@ -6,6 +6,8 @@ const PackagesSchema = new mongoose.Schema(
     price: { type: Number, default: null },
     description: { type: Array, default: [] },
     totalposts: { type: String, required: true },
+    duration: { type: String, required: true, default: '2' },
+
     interval: { type: String, required: true },
   },
   {
