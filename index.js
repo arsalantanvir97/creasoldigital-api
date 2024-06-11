de7c570e3cf1f8968ffaf7cb58c125b7cb6e3b81
Arsalan Tanveer <arsalantanvir3@gmail.com>
2024-06-11 17:26:36 +0200

    app listen error resolve

--- index.js
+++ index.js
@@ -9,22 +9,7 @@ const port = 4001
 const local = false
 let credentials = {}
 
-if (local) {
-  credentials = {
-    key: fs.readFileSync("/etc/apache2/ssl/onlinetestingserver.key", "utf8"),
-    cert: fs.readFileSync("/etc/apache2/ssl/onlinetestingserver.crt", "utf8"),
-    ca: fs.readFileSync("/etc/apache2/ssl/onlinetestingserver.ca"),
-  }
-} else {
-  credentials = {
-    key: fs.readFileSync("../certs/ssl.key"),
-    cert: fs.readFileSync("../certs/ssl.crt"),
-    ca: fs.readFileSync("../certs/ca-bundle"),
-  }
-}
-
-const httpsServer = https.createServer(credentials, app)
-httpsServer.listen(port, () => {
+app.listen(port, () => {
   console.log(
     "\u001b[" + 34 + "m" + `Server started on port: ${port}` + "\u001b[0m"
   )
